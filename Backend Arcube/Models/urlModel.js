c959b71ac9ce3e398754b151c35229a8f520e379
smida-ibtissem <smd.ibtissem@gmail.com>
2025-02-02 18:09:57 +0100

    Correction

--- Backend Arcube/Models/urlModel.js
+++ Backend Arcube/Models/urlModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const UrlSchema = new mongoose.Schema({
-  originalUrl: { type: String, required: true },
+  longUrl: { type: String, required: true },
   shortId: { type: String, required: true, unique: true },
   createdAt: { type: Date, default: Date.now },
 });
--- Backend Arcube/Routes/URLRoute.js
+++ Backend Arcube/Routes/URLRoute.js
@@ -6,30 +6,38 @@ const router = express.Router();
 
 // POST /shorten - Shorten a URL
 router.post("/shorten", async (req, res) => {
-  const { originalUrl } = req.body;
-  if (!originalUrl) return res.status(400).json({ error: "URL is required" });
+  const { longUrl } = req.body;
+  if (!longUrl) return res.status(400).json({ error: "URL is required" });
 
   try {
-    let url = await Url.findOne({ originalUrl });
+    let url = await Url.findOne({ longUrl });
 
     if (!url) {
       const shortId = shortid.generate();
-      url = new Url({ originalUrl, shortId });
+      console.log(shortId)
+      url = new Url({ longUrl, shortId });
+      console.log(url)
       await url.save();
+      console.log("Hello")
+
     }
 
+
     res.json({ shortUrl: `${process.env.BASE_URL}/${url.shortId}` });  } catch (err) {
+    console.error("Database Save Error:", err);
     res.status(500).json({ error: "Server error" });
   }
+  
 });
 
 // GET /:shortId - Redirect to the original URL
+
 router.get("/:shortId", async (req, res) => {
   try {
     const url = await Url.findOne({ shortId: req.params.shortId });
 
     if (url) {
-      return res.redirect(url.originalUrl);
+      return res.redirect(url.longUrl);
     } else {
       return res.status(404).json({ error: "URL not found" });
     }
